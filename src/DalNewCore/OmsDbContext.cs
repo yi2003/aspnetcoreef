7735b98f34fb77ac37a5839d46f3d2fc65c1916f
yi <yi@DESKTOP-S8BBF2N>
2016-10-10 14:41:03 +0800

    Add project files.

---
+++ src/DalNewCore/OmsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelCo;
+
+namespace DalCore
+{
+    public class OmsDbContext: Microsoft.EntityFrameworkCore.DbContext
+
+    {
+        public OmsDbContext(DbContextOptions<OmsDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        protected override  void OnModelCreating(ModelBuilder mb)
+        {
+
+            mb.Entity<Order>().ToTable("Orders");
+        }
+    }
+
+    
+}
---
+++ src/ServiceNewCore/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using ModelCo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DalCore;
+
+namespace ServiceNewCore
+{
+    public interface IOrderService
+    {
+        IList<Order> ListOrders();
+    }
+
+    public class OrderService : IOrderService
+    {
+
+
+        private OmsDbContext dbContext;
+
+        public OrderService(OmsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        public IList<Order> ListOrders()
+        {
+            return dbContext.Orders.Where(c=>c.Id ==1 ).ToList();
+        }
+    }
+}
